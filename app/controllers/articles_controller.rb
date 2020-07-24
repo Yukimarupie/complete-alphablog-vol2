060ee7ab960c3b791b4792d77f52309626d9bf0c
Yuki Harada <y.harada59@gmail.com>
2020-07-24 23:04:43 +0900

    added mainly action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,5 +7,34 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
-    
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+
+    def create
+        #render plain: params[:article]
+        #↑だと、/new でsubmitボタンが押された時、フォームに入力した値をそのまま出力するようになる。
+        #@article = Article.new(params[:article])
+        #↑だと、strongパラメータが設定されていないのでsubmitボタンが押されるとエラーが出る
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+        #render plain: @article.inspect #このinspexctがないと、titleやdescriptionが表示されない
+        #redirect_to article_path(@article)#rails routesで確認して、URIが/articles/:id(.:format) になってるから@articleも入れる。
+        flash[:notice] = "Article was created successfully"
+        redirect_to @article
+        else
+            render 'new'
+        end
+
+    end
+
+    def update
+
+    end
 end
