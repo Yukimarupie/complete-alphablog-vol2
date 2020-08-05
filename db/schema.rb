0b7547c5a556f65c1d2d64d1fe97e95d51c5ae6b
Yuki Harada <y.harada59@gmail.com>
2020-08-05 23:53:35 +0900

    add lec136 bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200729020624) do
+ActiveRecord::Schema.define(version: 20200805120507) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20200729020624) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
